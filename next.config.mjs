c7ec72aae57f21cfa88d251aae0ea3bfb6f7b3f7
palak40 <palaksharma03066@gmail.com>
2024-12-06 15:59:03 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    assetPrefix: './',  // This is necessary for GitHub Pages deployment
-  };
-  
-  export default nextConfig;
-  
+  reactStrictMode: true,
+  assetPrefix: '/my-nextjs-blog/',  // Adjust to your GitHub repository name
+  basePath: '/blog-nextjs-app',      // Ensure basePath is set if necessary
+};
+
+export default nextConfig;
